322922a029556ffd02a40718909844d369d66c1a
JinuBaeG <junchanam@naver.com>
2024-06-10 18:44:54 +0900

    Camera System Update

--- Assets/PROJECT UTD/Scripts/Camera System/QuaterViewCameraSystem.cs
+++ Assets/PROJECT UTD/Scripts/Camera System/QuaterViewCameraSystem.cs
@@ -37,22 +37,22 @@ namespace SCU
             cameraForward.y = 0;
             cameraRight.y = 0;
 
-            if (Input.GetKey(KeyCode.W))
+            if (Input.GetKey(KeyCode.UpArrow))
             {
                 cameraPivot.Translate(cameraForward * cameraMoveSpeed  * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.S))
+            if (Input.GetKey(KeyCode.DownArrow))
             {
                 cameraPivot.Translate(cameraForward * cameraMoveSpeed * (-1) * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.A))
+            if (Input.GetKey(KeyCode.LeftArrow))
             {
                 cameraPivot.Translate(cameraRight * cameraMoveSpeed * (-1) * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.RightArrow))
             {
                 cameraPivot.Translate(cameraRight * cameraMoveSpeed  * Time.deltaTime, Space.World);
             }
