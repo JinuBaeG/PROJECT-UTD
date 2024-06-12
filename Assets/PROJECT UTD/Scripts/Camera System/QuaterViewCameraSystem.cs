fa87a13e2a3a127fa9cd902c81a7016888919739
JinuBaeG <junchanam@naver.com>
2024-06-12 18:49:11 +0900

    Add Game Package

--- Assets/PROJECT UTD/Scripts/Camera System/QuaterViewCameraSystem.cs
+++ Assets/PROJECT UTD/Scripts/Camera System/QuaterViewCameraSystem.cs
@@ -8,10 +8,17 @@ namespace SCU
     {
         public float cameraRotateSpeed = 50f;
         public float cameraMoveSpeed = 50f;
+        public float zoomSensitvity = 10f;
 
         public Camera mainCamera;
         public Transform cameraPivot;
         public Cinemachine.CinemachineVirtualCamera quaterViewCamera;
+
+        public Cinemachine.CinemachineComponentBase componentBase;
+
+        float cameraDistance;
+        private float cameraMaxDistance = 100.0f;
+        private float cameraMinDistance = 5.0f;
 
         private void Update()
         {
@@ -37,24 +44,45 @@ namespace SCU
             cameraForward.y = 0;
             cameraRight.y = 0;
 
-            if (Input.GetKey(KeyCode.UpArrow))
+            if (Input.GetKey(KeyCode.W))
             {
                 cameraPivot.Translate(cameraForward * cameraMoveSpeed  * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.DownArrow))
+            if (Input.GetKey(KeyCode.S))
             {
                 cameraPivot.Translate(cameraForward * cameraMoveSpeed * (-1) * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.LeftArrow))
+            if (Input.GetKey(KeyCode.A))
             {
                 cameraPivot.Translate(cameraRight * cameraMoveSpeed * (-1) * Time.deltaTime, Space.World);
             }
 
-            if (Input.GetKey(KeyCode.RightArrow))
+            if (Input.GetKey(KeyCode.D))
             {
                 cameraPivot.Translate(cameraRight * cameraMoveSpeed  * Time.deltaTime, Space.World);
+            }
+
+            if(componentBase == null)
+            {
+                componentBase = quaterViewCamera.GetCinemachineComponent(Cinemachine.CinemachineCore.Stage.Body);
+            }
+
+            if (Input.GetAxis("Mouse ScrollWheel") != 0)
+            {
+                cameraDistance = Input.GetAxis("Mouse ScrollWheel") * zoomSensitvity;
+                if(componentBase is Cinemachine.CinemachineFramingTransposer)
+                {
+                    (componentBase as Cinemachine.CinemachineFramingTransposer).m_CameraDistance -= cameraDistance;
+                    if((componentBase as Cinemachine.CinemachineFramingTransposer).m_CameraDistance > cameraMaxDistance) {
+                        (componentBase as Cinemachine.CinemachineFramingTransposer).m_CameraDistance = cameraMaxDistance;
+                    } else if ((componentBase as Cinemachine.CinemachineFramingTransposer).m_CameraDistance < cameraMinDistance)
+                    {
+                        (componentBase as Cinemachine.CinemachineFramingTransposer).m_CameraDistance = cameraMinDistance;
+                    }
+                            
+                }
             }
 
         }
